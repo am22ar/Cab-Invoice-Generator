0dd25f34ec6283e38779c0d26e01ed65bc196f05
am22ar <amarpatil12222@gmail.com>
2022-11-16 14:45:30 +0530

    Added: enhance generator UC3

--- src/main/java/Ride.java
+++ src/main/java/Ride.java
@@ -1,11 +1,11 @@
 
 public class Ride {
 	public final int time;
-	public double distance;
-
-	public Ride(double distance, int time) {
-		this.distance = distance;
-		this.time = time;
-	}
+    public double distance;
+    public Ride(double distance, int time){
+        this.distance = distance;
+        this.time = time;
+    }
+	
 
 }
